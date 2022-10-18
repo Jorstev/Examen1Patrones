5caa048d877aec785e443340d55209ddaa6d14d0
Jorstev <71983628+Jorstev@users.noreply.github.com>
2022-10-18 16:00:29 -0600

    Initial Commit
    Initial Commit

---
+++ PatronesExamen1/src/main/java/PatronesExamen1/PatronesExamen1/PatronesExamen1Application.java
@@ -0,0 +1,13 @@
+package PatronesExamen1.PatronesExamen1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronesExamen1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronesExamen1Application.class, args);
+	}
+
+}
